852f12847a79c9a819f74fd45e79e9257ed11e84
Shafi <shafiyaftali001@gmail.com>
2024-01-12 16:23:07 -0500

    Added New Methods

--- Packages/main/Main.java
+++ Packages/main/Main.java
@@ -9,6 +9,9 @@ public class Main {
     account.accountHolder = "Alex";
     account.accountNumber = 123456789;
     account.balance = 1000000.40;
-    
+
+    System.out.println("Account Holder Name: " + account.accountHolder);
+    System.out.println("Account Number: " + account.accountNumber);
+    System.err.println("Account Balance: " + account.balance);
   }
 }
