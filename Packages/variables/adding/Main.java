550f260b4d21690139f03195cbf829176c8bd56a
Shafi <shafiyaftali001@gmail.com>
2024-01-14 01:20:23 -0500

    feat: Add adding file in Packages! #Shafi

---
+++ Packages/variables/adding/Main.java
@@ -0,0 +1,10 @@
+package variables.adding;
+
+public class Main {
+  public static void main(String[] args){
+    int numOne = 6;
+    int numTwo = 10;
+    int addTwoNum = numOne + numTwo;
+    System.out.println("Result: " + addTwoNum);
+  }
+}
