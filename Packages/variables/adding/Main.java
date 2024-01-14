81c4e8d3b5c12c9253ea3a8452550cd47d32c3ac
Shafi <shafiyaftali001@gmail.com>
2024-01-14 11:38:57 -0500

    feat: Add float variables in adding file

--- Packages/variables/adding/Main.java
+++ Packages/variables/adding/Main.java
@@ -4,7 +4,12 @@ public class Main {
   public static void main(String[] args){
     int numOne = 6;
     int numTwo = 10;
+    float numThre = 30300;
+    float numFour = 44;
+    float dividFloats = numThre / numFour;
     int addTwoNum = numOne + numTwo;
     System.out.println("Result: " + addTwoNum);
+    System.out.println("Result: " + dividFloats);
+
   }
 }
