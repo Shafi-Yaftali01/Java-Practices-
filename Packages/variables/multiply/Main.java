19b32a58e211de0976b21b68a7c7c1bd8475a996
Shafi <shafiyaftali001@gmail.com>
2024-01-15 15:20:15 -0500

    feat: add a new file on adding file which multiply two int number.

---
+++ Packages/variables/multiply/Main.java
@@ -0,0 +1,13 @@
+package variables.multiply;
+
+public class Main {
+  public static void main(String[] args) {
+    int numOne = 45;
+    int numTwo = 54;
+    int result = numOne * numTwo;
+
+    System.out.println("Result: " + result);
+    
+
+  }
+}
