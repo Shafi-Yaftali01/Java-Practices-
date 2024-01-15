9d5d5ecb9dc16238b7b1fad6b2d762624bba1b47
Shafi <shafiyaftali001@gmail.com>
2024-01-15 18:49:53 -0500

    feat: add a new file object that contains primitive

---
+++ Packages/object/primitive/Main.java
@@ -0,0 +1,19 @@
+package object.primitive;
+
+public class Main {
+  public static void main(String[] args) {
+    Person person = new Person();
+    person.name = "Jim";
+    person.age = 20;
+
+  }
+}
+/**
+ * InnerPerson 
+ */
+ class Person {
+  String name;
+  int age;
+
+  
+}
