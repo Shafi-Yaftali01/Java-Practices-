fdf62731c0343427932afeb7e1988e1620abb6c7
Shafi <shafiyaftali001@gmail.com>
2024-01-15 15:25:39 -0500

    feat: add file on variable file: lenghtOfString

---
+++ Packages/variables/lengthOfString/Main.java
@@ -0,0 +1,10 @@
+package variables.lengthOfString;
+
+public class Main {
+  public static void main(String[] args) {
+    String nameLenght = "Alex";
+    int result = nameLenght.length();
+    System.out.println("The Lenght of name is: " + result);
+    
+  }
+}
