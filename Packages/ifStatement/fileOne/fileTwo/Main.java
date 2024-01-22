f0ff90df8b80001216e95d8b88900fe0adb06955
Shafi <shafiyaftali001@gmail.com>
2024-01-22 12:39:36 -0500

    add file Two
    used if statements
    
    Co-Authored-By: Iqbal_Daudzai <143276548+daudzai01@users.noreply.github.com>

---
+++ Packages/ifStatement/fileOne/fileTwo/Main.java
@@ -0,0 +1,18 @@
+package ifStatement.fileOne.fileTwo;
+
+public class Main {
+  public static void main(String[] args) {
+    char grades = 'A';
+
+    if (grades == 'A') {
+      System.out.println("You are the best!");
+
+    }
+
+    else {
+      System.out.println("Learn More:)");
+      
+    }
+
+  }
+}
