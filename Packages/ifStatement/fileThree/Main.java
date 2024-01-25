92f486ef42380549c9c769153e73ece5339bb335
Shafi <shafiyaftali001@gmail.com>
2024-01-25 00:37:11 -0500

    feat: add file three on ifStatement file

---
+++ Packages/ifStatement/fileThree/Main.java
@@ -0,0 +1,13 @@
+package ifStatement.fileThree;
+
+public class Main {
+
+  public static void main(String[] args) {
+    int[] arrayOfNUmbers = {1,2,3,4,5};
+
+    for (int i = 0; i < arrayOfNUmbers.length; i++) {
+      System.out.println("Value at index: " + i + ": " + arrayOfNUmbers[i]);
+      
+    }
+  }
+}
