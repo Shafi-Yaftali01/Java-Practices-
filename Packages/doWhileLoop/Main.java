1ab4ae9f243463c20587c83827325b21c1f1fbc6
Shafi <shafiyaftali001@gmail.com>
2024-01-25 21:27:14 -0500

    feat: final code doWhileLoop

---
+++ Packages/doWhileLoop/Main.java
@@ -0,0 +1,13 @@
+package doWhileLoop;
+
+public class Main {
+  public static void main(String[] args) {
+    int numbers = 10;
+    int index = 1;
+    do {
+      System.out.println(index);
+      index++;
+    }while(index <= numbers);
+
+  }
+}
