ac0ca1140d089b0ef3e1675a08beaf8a003f6295
Shafi <shafiyaftali001@gmail.com>
2024-01-31 23:40:05 -0500

    feat: add file basic Method

---
+++ Packages/basicMethod/Main.java
@@ -0,0 +1,13 @@
+package basicMethod;
+
+public class Main {
+  public static void main(String[] args) {
+    String userName = "Shafi";
+    gree(userName);
+
+  }
+
+  public static void gree(String name) {
+    System.out.println("Welcome " + name);
+  }
+}
