38a96aa2a637f55b76e0f4a32bcc2b4ba03ac77c
Shafi <shafiyaftali001@gmail.com>
2024-01-31 23:44:30 -0500

    feat: add input method file

---
+++ Packages/inputMethod/Main.java
@@ -0,0 +1,17 @@
+package inputMethod;
+
+public class Main {
+  public static void main(String[] args) {
+    // add two number 
+    int numOne = 10;
+    int numTwo = 5;
+    addTwoNumber(numOne, numTwo);
+  }
+
+  public static void addTwoNumber(int num1, int num2) {
+    // add numbers 
+
+    int result = num1 + num2;
+    System.out.println("Result: " + result);
+  }
+}
