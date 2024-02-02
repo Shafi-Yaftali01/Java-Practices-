3eb49dd46c24b9d87d7d460c69d3403709d1c67d
Shafi <shafiyaftali001@gmail.com>
2024-02-01 22:06:41 -0500

    feat: add final code

---
+++ Packages/multiplyMethod/Main.java
@@ -0,0 +1,13 @@
+package multiplyMethod;
+
+class Main {
+  public static void main(String[] args){
+
+    int result = multipl(4, 3);
+    System.out.println(result);
+  }
+
+  public static int multipl(int a, int b) {
+    return a * b; 
+  }
+}
