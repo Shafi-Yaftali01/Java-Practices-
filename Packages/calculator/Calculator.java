4a6cfe927c8ac01ffd38cac09e051a3fa339e81a
Shafi <shafiyaftali001@gmail.com>
2024-02-03 10:16:40 -0500

    feat: add another function in Calculator file

--- Packages/calculator/Calculator.java
+++ Packages/calculator/Calculator.java
@@ -4,4 +4,7 @@ public class Calculator {
   public static int add(int a, int b) {
     return a + b; 
   }
+  public static double multiply(double numberOne, double numberTwo) {
+    return numberOne * numberTwo; 
+  }
 }
--- Packages/calculator/Main.java
+++ Packages/calculator/Main.java
@@ -4,5 +4,10 @@ public class Main {
   public static void main(String[] args) {
     int sum = Calculator.add(3,4);
     System.out.println("Sum: " + sum);
+
+    // Invoking the function from another class
+    double multiply = Calculator.multiply(5.01, 10.2);
+    System.out.println("Result: " + multiply);
+    
   }
 }
