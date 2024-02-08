7ab1af9ddfb0c8a0ade7fdcdf5acf3bf39579dd5
Shafi <shafiyaftali001@gmail.com>
2024-02-07 22:42:52 -0500

    feat: access file with using person file and Main to make practice the access modifiers in java

---
+++ Packages/access/Main.java
@@ -0,0 +1,11 @@
+package access;
+
+public class Main {
+  public static void main(String[] args) {
+    Person person = new Person();
+    System.out.println("Name: " + person.name);
+    System.out.println("Age: " + person.age);
+    System.out.println("Address: " + person.address);
+
+  }
+}
---
+++ Packages/access/Person.java
@@ -0,0 +1,8 @@
+package access;
+
+public class Person {
+  public String name = "Shafi";
+  protected int age  = 17;
+  String address = "369 Mars Near Beach";
+  
+}
