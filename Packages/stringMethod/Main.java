13d582e92417ae805978051d255db1a9834587fe
Shafi <shafiyaftali001@gmail.com>
2024-02-22 13:20:15 -0500

    feat: added the lowercase, substring, replace method on string

--- Packages/stringMethod/Main.java
+++ Packages/stringMethod/Main.java
@@ -5,5 +5,8 @@ public class Main {
     String string = "Hello World";
     System.out.println("Length: " + string.length());
     System.out.println("Uppercase: " + string.toUpperCase());
+    System.out.println("Lowercase: " + string.toLowerCase());
+    System.out.println("Substring:" + string.substring(2, 7));
+    System.out.println("Replace" + string.replace("Hello", "Hey"));
   }
 }
