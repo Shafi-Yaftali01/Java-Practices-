b59948ca9a377a067f92fff9b0a7ef167a9e35c3
Shafi <shafiyaftali001@gmail.com>
2024-02-22 13:08:27 -0500

    feat: string lessons

---
+++ Packages/stringClass/Main.java
@@ -0,0 +1,10 @@
+package stringClass;
+
+public class Main {
+  public static void main(String[] args) {
+    String stringOne = "Hello"; 
+    String stringTwo = "world";
+    System.out.println(stringOne + stringTwo);
+    
+  }
+}
