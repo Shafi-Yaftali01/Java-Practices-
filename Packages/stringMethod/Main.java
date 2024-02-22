a018d07fbbd06b1b49719e178a73881b14795995
Shafi <shafiyaftali001@gmail.com>
2024-02-22 13:17:39 -0500

    feat: added the methods for string

---
+++ Packages/stringMethod/Main.java
@@ -0,0 +1,9 @@
+package stringMethod;
+
+public class Main {
+  public static void main(String[] args) {
+    String string = "Hello World";
+    System.out.println("Length: " + string.length());
+    System.out.println("Uppercase: " + string.toUpperCase());
+  }
+}
