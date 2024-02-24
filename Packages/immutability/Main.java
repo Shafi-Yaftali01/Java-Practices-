e4f3ae4feb3f7c13613bceb805fd87ac97ad1a15
Shafi <shafiyaftali001@gmail.com>
2024-02-23 21:31:05 -0500

    add: immutiability with String in Java

---
+++ Packages/immutability/Main.java
@@ -0,0 +1,10 @@
+package Packages.immutability;
+
+class Main {
+public static void main(String[] args) {
+  String str = "Hello, World";
+  String toUpperCase = str.toUpperCase();
+  System.out.println("Original String: " + str);
+  System.out.println("Modified String: " + toUpperCase);
+} 
+}
